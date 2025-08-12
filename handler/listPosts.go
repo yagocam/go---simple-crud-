10999eacfafec99560ae24f51ee0ba8e5706c64e
yagocam <yagocam5@gmail.com>
2025-08-12 15:03:02 -0300

    feat: creating list posts

--- handler/listPosts.go
+++ handler/listPosts.go
@@ -1,13 +1,23 @@
 package handler
 
 import (
+	"awesomeProject/schemas"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func ListPostHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusCreated, gin.H{
-		"message": "POST posts",
-	})
+
+	var posts []schemas.Post
+	if err := db.Find(&posts).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	var response []PostResponse
+	for _, p := range posts {
+		response = append(response, ToPostResponse(p))
+	}
+
+	ctx.JSON(http.StatusOK, response)
 }
