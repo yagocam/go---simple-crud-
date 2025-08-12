8350db1c6f4a40dc92ecaa178541b929a6221b22
yagocam <yagocam5@gmail.com>
2025-08-12 15:37:03 -0300

    feat: starting update

--- handler/updatePost.go
+++ handler/updatePost.go
@@ -1,13 +1,28 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UpdatePostHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusCreated, gin.H{
-		"message": "POST posts",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Id obrigatorio"})
+		return
+	}
+	request := UpdatePostRequest{}
+
+	if err := db.First(&post, id).Error; err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{fmt.Sprintf("Id não encontrado %s", id): id})
+		return
+	}
+	if err := db.Updates(&post).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{fmt.Sprintf("Error delete post %s", id): id})
+		return
+	}
+
+	sendSuccess(ctx, "delete-post", ToPostResponse(post))
 }
