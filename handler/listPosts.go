e6bf66296003c8cb0ee27922e167bc22636f42f6
yagocam <yagocam5@gmail.com>
2025-08-12 15:23:54 -0300

    feat: applying tags filter

--- handler/listPosts.go
+++ handler/listPosts.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"awesomeProject/schemas"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,7 +11,20 @@ import (
 func ListPostHandler(ctx *gin.Context) {
 
 	var posts []schemas.Post
-	if err := db.Find(&posts).Error; err != nil {
+	tagsParam := ctx.Query("tags")
+	var tags []string
+	if tagsParam != "" {
+		tags = strings.Split(tagsParam, ",")
+	}
+
+	query := db.Model(&schemas.Post{})
+
+	if len(tags) > 0 {
+		for _, tag := range tags {
+			query = query.Where("tags LIKE ?", "%"+tag+"%")
+		}
+	}
+	if err := query.Find(&posts).Error; err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err})
 		return
 	}
--- schemas/post.go
+++ schemas/post.go
@@ -1,14 +1,13 @@
 package schemas
 
 import (
-	"github.com/lib/pq"
 	"gorm.io/gorm"
 )
 
 type Post struct {
 	gorm.Model
-	Title   string         `json:"title"`
-	Author  string         `json:"author"`
-	Content string         `json:"content"`
-	Tags    pq.StringArray `gorm:"type:text[]" json:"tags"`
+	Title   string   `json:"title"`
+	Author  string   `json:"author"`
+	Content string   `json:"content"`
+	Tags    []string `gorm:"serializer:json" json:"tags"`
 }
