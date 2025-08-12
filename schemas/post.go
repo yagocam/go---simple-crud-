9c8df71892727bd8e24d5acf421dbf204aafe556
yagocam <yagocam5@gmail.com>
2025-08-12 14:52:02 -0300

    fix: fixing data type

--- schemas/post.go
+++ schemas/post.go
@@ -1,13 +1,14 @@
 package schemas
 
 import (
+	"github.com/lib/pq"
 	"gorm.io/gorm"
 )
 
 type Post struct {
 	gorm.Model
-	Title   string   `json:"title"`
-	Author  string   `json:"author"`
-	Content string   `json:"content"`
-	Tags    []string `gorm:"type:text" json:"tags" gorm:"serializer:json"`
+	Title   string         `json:"title"`
+	Author  string         `json:"author"`
+	Content string         `json:"content"`
+	Tags    pq.StringArray `gorm:"type:text[]" json:"tags"`
 }
