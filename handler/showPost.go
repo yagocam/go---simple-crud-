999e7c36bbd4d341cbf33901e7383b2f78725784
yagocam <yagocam5@gmail.com>
2025-08-12 15:36:51 -0300

    feat: show post

--- handler/showPost.go
+++ handler/showPost.go
@@ -1,13 +1,25 @@
 package handler
 
 import (
+	"awesomeProject/schemas"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func ShowPostHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusCreated, gin.H{
-		"message": "POST posts",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Id obrigatorio"})
+		return
+	}
+
+	post := schemas.Post{}
+	if err := db.First(&post, id).Error; err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{fmt.Sprintf("Id não encontrado %s", id): id})
+		return
+	}
+
+	sendSuccess(ctx, "show-post", ToPostResponse(post))
 }
