cf54bc0822fe8975b155ec72c3d12d1e8ba21b7d
yagocam <yagocam5@gmail.com>
2025-08-13 22:26:21 -0300

    feat: adding update handler

--- handler/updatePost.go
+++ handler/updatePost.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"awesomeProject/schemas"
 	"fmt"
 	"net/http"
 
@@ -14,15 +15,20 @@ func UpdatePostHandler(ctx *gin.Context) {
 		return
 	}
 	request := UpdatePostRequest{}
-
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		errors := GetValidationErrors(err)
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"error": errors})
+		return
+	}
+	post := schemas.Post{}
 	if err := db.First(&post, id).Error; err != nil {
 		ctx.JSON(http.StatusNotFound, gin.H{fmt.Sprintf("Id não encontrado %s", id): id})
 		return
 	}
-	if err := db.Updates(&post).Error; err != nil {
+	if err := db.Updates(&request).Error; err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{fmt.Sprintf("Error delete post %s", id): id})
 		return
 	}
 
-	sendSuccess(ctx, "delete-post", ToPostResponse(post))
+	sendSuccess(ctx, "update-post", ToPostResponse(post))
 }
